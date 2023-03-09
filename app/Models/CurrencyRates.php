80028ca0f1288f351449d9e04c42515a6cd23e62
Tobiasz Czelakowski <tobiasz@czelakowski.pl>
2023-03-09 12:07:08 +0100

    UUID implementation instead simple id

--- app/Models/CurrencyRates.php
+++ app/Models/CurrencyRates.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CurrencyRates extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $table = 'rates';
     protected $fillable = ['name', 'currency_code', 'exchange_rate'];
--- database/migrations/2023_03_02_000913_create_rates_table.php
+++ database/migrations/2023_03_02_000913_create_rates_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('rates', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('currency_code');
             $table->decimal('exchange_rate', 5, 2);
